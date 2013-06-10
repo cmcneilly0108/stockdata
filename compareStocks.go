6d5440ce914b93f53695d07b639891be09155d28
Chris McNeilly <cmcneilly@motistainsights.com>
2013-06-10 14:53:23 -0700

    some reg exp working

--- compareStocks.go
+++ compareStocks.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+        "strconv"
         "fmt"
         "net/http"
         "io/ioutil"
         "regexp"
-//        "strings"
+        "os"
 )
 
-// http://finance.yahoo.com/d/quotes.csv?s=bwld+ctsh&f=snl1
+type ststats struct {
+	ticker string
+	score int
+	pegRatio float64
+	pMargin float64
+	revenueGrowth float64
+	yrChange float64
+	spyrChange float64
+}
 
+func convertToFloat(numString string) float64 {
+	var result float64
+	if numString[len(numString)-1] == '%' {
+		numString = numString[0:len(numString)-1]
+		result, _ = strconv.ParseFloat(numString,64)
+		result = result/100
+	} else {
+		result, _ = strconv.ParseFloat(numString,64)
+	}
+	return result
+}
+
+// populate fields in structure for one ticker
+// pass tickers in via command line
+// loop through, populating the full structure
+// score each ticker
 func main() {
+        argsWithoutProg := os.Args[1:]
+        fmt.Println(argsWithoutProg)
+        // Profit Margin (ttm):</td><td class="yfnc_tabledata1">21.58%</td>
+        //r := regexp.MustCompile("Profit Margin (ttm):</td><td [^>].>([^<]+)</td>")
+        pmre := regexp.MustCompile("Profit Margin.*?</td><td.*?>(.*?)</td>")
+        prre := regexp.MustCompile("PEG Ratio.*?</td><td.*?>(.*?)</td>")
+
+
+
         //res, err := http.Get("http://finance.yahoo.com/d/quotes.csv?s=bwld+ctsh+cybx+dar+jazz+mwiv+pcp+pets+rex+voxx&f=snl1")
         res, err := http.Get("http://finance.yahoo.com/q/ks?s=msft")
         if err != nil {
@@ -24,13 +58,16 @@ func main() {
                 fmt.Println("ioutil.ReadAll", err)
                 return
         }
-        //Convert the byte array into each line, and convert to a string
-        //fmt.Println(string(body))
-        // Profit Margin (ttm):</td><td class="yfnc_tabledata1">21.58%</td>
-        //r := regexp.MustCompile("Profit Margin (ttm):</td><td [^>].>([^<]+)</td>")
-        r := regexp.MustCompile("Profit Margin[^<]+</td><td [^>]+>([^<]+)")
-        strs := r.FindSubmatch(body)
-        fmt.Println(string(strs[1]))
+        stock1 := ststats{"msft",0,0,0,0,0,0}
+
+        strs := pmre.FindSubmatch(body)
+	stock1.pMargin = convertToFloat(string(strs[1]))
+	
+        strs = prre.FindSubmatch(body)
+	stock1.pegRatio = convertToFloat(string(strs[1]))
+	
+	
+	fmt.Println(stock1)
 	/*for _, line := range strings.Split(string(body), "\n") {
 		if (len(line) > 0) {
 			// TBD - parse CSV line into fields, store in DB
