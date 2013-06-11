e5084f41b4202d95594add5c9ebc59c59f938683
Chris McNeilly <cmcneilly0108@gmail.com>
2013-06-10 19:16:56 -0700

    scoring and arg1 working

--- compareStocks.go
+++ compareStocks.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-        "strconv"
-        "fmt"
-        "net/http"
-        "io/ioutil"
-        "regexp"
-        "os"
-)
+	"strconv"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"regexp"
+	"os"
+	)
 
 type ststats struct {
 	ticker string
@@ -22,6 +22,43 @@ type ststats struct {
 
 func calculateStockScore(st ststats) int {
 	score := 0
+	if (st.pegRatio < 1) {
+		score++
+	}
+	if (st.pegRatio < 1.5) {
+		score++
+	}
+	if (st.pegRatio > 2.5) {
+		score--
+	}
+	if (st.pMargin > .1) {
+		score++
+	}
+	if (st.pMargin > .3) {
+		score++
+	}
+	if (st.pMargin < 0) {
+		score--
+	}
+	if (st.revenueGrowth > .1) {
+		score++
+	}
+	if (st.revenueGrowth > .3) {
+		score++
+	}
+	if (st.revenueGrowth < 0) {
+		score--
+	}
+	if (st.netChange > 0) {
+		score++
+	}
+	if (st.netChange > .1) {
+		score++
+	}
+	if (st.netChange < 0) {
+		score--
+	}
+	
 	return score
 }
 
@@ -37,53 +74,56 @@ func convertToFloat(numString string) float64 {
 	return result
 }
 
-// score each ticker
 // pass tickers in via command line
 // loop through, populating the full structure for all tickers
 func main() {
-        argsWithoutProg := os.Args[1:]
-        fmt.Println(argsWithoutProg)
-        // Profit Margin (ttm):</td><td class="yfnc_tabledata1">21.58%</td>
-        //r := regexp.MustCompile("Profit Margin (ttm):</td><td [^>].>([^<]+)</td>")
-        pmre := regexp.MustCompile("Profit Margin.*?</td><td.*?>(.*?)</td>")
-        prre := regexp.MustCompile("PEG Ratio.*?</td><td.*?>(.*?)</td>")
-        rgre := regexp.MustCompile("Qtrly Revenue.*?</td><td.*?>(.*?)</td>")
-        ycre := regexp.MustCompile("52-Week Change.*?</td><td.*?>(.*?)</td>")
-        scre := regexp.MustCompile("P500 52-Week Change.*?</td><td.*?>(.*?)</td>")
-
-
-
-        //res, err := http.Get("http://finance.yahoo.com/d/quotes.csv?s=bwld+ctsh+cybx+dar+jazz+mwiv+pcp+pets+rex+voxx&f=snl1")
-        res, err := http.Get("http://finance.yahoo.com/q/ks?s=goog")
-        if err != nil {
-                fmt.Println("http.Get", err)
-                return
-        }
-        defer res.Body.Close()
-        //Grab the results from the call into a byte array?
-        body, err := ioutil.ReadAll(res.Body)
-        if err != nil {
-                fmt.Println("ioutil.ReadAll", err)
-                return
-        }
-        stock1 := ststats{"goog",0,0,0,0,0,0,0}
-
-        strs := pmre.FindSubmatch(body)
+	argsWithoutProg := os.Args[1:]
+	fmt.Println(argsWithoutProg)
+	ticker := argsWithoutProg[0]
+	url := "http://finance.yahoo.com/q/ks?s=" + ticker
+	// Profit Margin (ttm):</td><td class="yfnc_tabledata1">21.58%</td>
+	//r := regexp.MustCompile("Profit Margin (ttm):</td><td [^>].>([^<]+)</td>")
+	pmre := regexp.MustCompile("Profit Margin.*?</td><td.*?>(.*?)</td>")
+	prre := regexp.MustCompile("PEG Ratio.*?</td><td.*?>(.*?)</td>")
+	rgre := regexp.MustCompile("Qtrly Revenue.*?</td><td.*?>(.*?)</td>")
+	ycre := regexp.MustCompile("52-Week Change.*?</td><td.*?>(.*?)</td>")
+	scre := regexp.MustCompile("P500 52-Week Change.*?</td><td.*?>(.*?)</td>")
+	
+	
+	
+	//res, err := http.Get("http://finance.yahoo.com/d/quotes.csv?s=bwld+ctsh+cybx+dar+jazz+mwiv+pcp+pets+rex+voxx&f=snl1")
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("http.Get", err)
+		return
+	}
+	defer res.Body.Close()
+	//Grab the results from the call into a byte array?
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("ioutil.ReadAll", err)
+		return
+	}
+	stock1 := ststats{ticker,0,0,0,0,0,0,0}
+	
+	strs := pmre.FindSubmatch(body)
 	stock1.pMargin = convertToFloat(string(strs[1]))
 	
-        strs = prre.FindSubmatch(body)
+	strs = prre.FindSubmatch(body)
 	stock1.pegRatio = convertToFloat(string(strs[1]))
 	
-        strs = rgre.FindSubmatch(body)
+	strs = rgre.FindSubmatch(body)
 	stock1.revenueGrowth = convertToFloat(string(strs[1]))
 	
-        strs = ycre.FindSubmatch(body)
+	strs = ycre.FindSubmatch(body)
 	stock1.yrChange = convertToFloat(string(strs[1]))
 	
-        strs = scre.FindSubmatch(body)
+	strs = scre.FindSubmatch(body)
 	stock1.spyrChange = convertToFloat(string(strs[1]))
 	
 	stock1.netChange = stock1.yrChange - stock1.spyrChange
+	
+	stock1.score = calculateStockScore(stock1)
 	
 	
 	fmt.Println(stock1)
@@ -93,5 +133,5 @@ func main() {
 			fmt.Println("line splitter =",line)
 	  	}
 	} */ 
-
+	
 }
