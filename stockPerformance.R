69c44408cabe4358581443c5b3bc7f761798bbea
Chris McNeilly <cmcneilly0108@gmail.com>
2014-03-24 21:43:13 -0700

    Track 9 month performance

---
+++ stockPerformance.R
@@ -0,0 +1,74 @@
+library(quantmod)
+library(plyr)
+library(ggplot2)
+library(scales)
+library(lubridate)
+# Get historical data for each of my 10 stocks
+holdings <- c('ALG','BWLD','DAR','JAZZ','MWIV','FLXS','PCCC','REGI','REX','VOXX','SPY')
+prospects <- c('SPY','KBALB','ISH','FLXS','SMP','WLFC','DCO')
+#t2 = c('VOXX')
+
+getPrices <- function(x){
+  prices <- as.data.frame(Cl(x))
+  prices$date <- as.Date(rownames(prices))
+  #print('here')
+  #prices$ticker <- factor('SPY') # Need ticker symbol
+  #print('h2')
+  colnames(prices)[1] <- 'close'
+  #print('h3')
+  rownames(prices) <- NULL
+  #print('Not here')
+  prices$date <- ymd(prices$date)
+  return(prices)
+}
+
+compareStocks <- function(tickers) {
+  # calculate start 1 year from today
+  #sDate <- today() - years(1)
+  sDate <- today() - months(9)
+  getSymbols(tickers,src='yahoo',from=as.character(sDate))
+  data <- mget(tickers)
+  allPrices <- ldply(data,getPrices)
+  colnames(allPrices) <- c('ticker','close','date')
+  print(head(allPrices))
+  
+  # Find Start price and append to rows
+  iPrice <- allPrices[allPrices$date==min(allPrices$date),]
+  iPrice <- subset(iPrice, select = -date)
+  colnames(iPrice) <- c('ticker','startPrice')
+  allPrices <- join(allPrices,iPrice)  
+  
+  # Scale the price by start price
+  # allPrices$newPrice <- (allPrices$close/allPrices$startPrice) - 1
+  allPrices$gain <- (allPrices$close/allPrices$startPrice) - 1
+  allPrices$gainP <- percent((allPrices$close/allPrices$startPrice) - 1)
+  
+  # In order to pick which one to sell
+  #last <- max(ymd(allPrices$date[!is.na(allPrices$date)]))
+  #first <- min(ymd(allPrices$date[!is.na(allPrices$date)]))
+  #m3 <- last - days(90)
+  #m6 <- last - days(180)
+  # Create df with tickers and purchase date
+  # Calculate gain for past year
+  # subset(allPrices,date==last | date==m6 | date==m3,select=c(date,ticker,gain))
+  results <- subset(allPrices,date==max(allPrices$date),select=c(date,ticker,gain,gainP))
+  # Sort results by gain descending
+  print(results[order(-results$gain),c('ticker','gainP')])
+}
+
+compareStocks(holdings)
+
+compareStocks(prospects)
+# Color code compared to SPY for same periods
+# Go back to original financial package and see how much of this it can do
+
+# Plot performance over past year
+#ggplot(allPrices,aes(x=date,y=close)) + 
+#  geom_line() + scale_y_log10() + facet_wrap(~ ticker)
+
+# Try plotting all 10 on same chart
+#ggplot(allPrices,aes(x=date,y=close,colour=ticker)) +
+#  geom_line() + scale_y_log10()
+
+# plot the log price - better but not easily comparable
+#ggplot(allPrices,aes(x=date,y=newPrice,colour=ticker)) + geom_line() + scale_y_log10()
